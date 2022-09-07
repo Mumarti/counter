557cda9f7e2f18f0242a4fb092b620089fe54a56
Mumarti <74790481+Mumarti@users.noreply.github.com>
2022-09-07 08:41:11 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,66 +1,75 @@
-let increase = document.getElementById('plus');
-let reset = document.getElementById('reset');
-let decrease = document.getElementById('minus');
-let counter = document.getElementById('score');
+let counter = document.getElementById('counter');
+let iconContainer = document.getElementById('icon_container');
 let image = document.getElementById('image_random');
-let score = 0;
 
-let image_array = [
-    '1.webp',
-    '2.webp',
-    '3.webp',
-    '4.webp',
-    '5.webp',
-    '6.webp',
-    '7.webp',
-    '8.webp',
-    '9.webp',
-    '10.webp',
-    '11.webp',
-    '12.webp',
-    '13.webp',
-    '14.webp',
-    '15.webp',
-    '16.webp',
-    '17.webp',
-    '18.webp',
-    '19.webp',
-    '20.webp',
-    '21.webp',
-    '22.webp',
-    '23.webp',
-    '24.webp',
-    '25.webp',
-    '26.webp',
-    '27.webp',
-    '28.webp',
-    '29.webp',
-    '30.webp',
-]
+//score
+let scoreContainer = document.createElement("p");
+let scoreText = document.createTextNode("0");
 
-    function get_random_image(){
-    let random_index = Math.floor(Math.random() * image_array.length);
-    let selected_image = image_array[random_index];
-    image.src = `./img/${selected_image}`
-    }
+scoreContainer.appendChild(scoreText);
+counter.appendChild(scoreContainer);
 
+scoreContainer.setAttribute("id", "score");
 
-    increase.addEventListener('click', get_random_image);
-    decrease.addEventListener('click', get_random_image);
-    reset.addEventListener('click', get_random_image);
+//plus icon
+let plus = document.createElement("i");
+plus.classList.add('fa-regular', 'fa-thumbs-up')
 
-increase.addEventListener('click', ()=>{
-    score++;
-    counter.innerText = score;
+iconContainer.appendChild(plus);
+
+//reset icon
+let reset = document.createElement("i");
+reset.classList.add('fa-solid', 'fa-repeat');
+
+iconContainer.appendChild(reset);
+
+//minus icon
+let minus = document.createElement("i");
+minus.classList.add('fa-regular', 'fa-thumbs-down');
+
+iconContainer.appendChild(minus);
+
+//counter
+
+let SCORE = 0;
+
+plus.addEventListener('click', ()=>{
+    SCORE++;
+    scoreContainer.innerHTML = SCORE;
 });
 
-decrease.addEventListener('click', ()=>{
-    score--;
-    counter.innerText = score;
+minus.addEventListener('click', ()=>{
+    SCORE--;
+    scoreContainer.innerHTML = SCORE;
 });
 
-reset.addEventListener('click', ()=>{
-    score = 0;
-    counter.innerText = score;
+reset.addEventListener('click', ()=> {
+    SCORE = 0;
+    scoreContainer.innerHTML = SCORE;
+
+    let resetTag = document.createElement('p');
+    resetTag.style.cssText = 'font-size:30px; color:#fff; background-color:#BE7F89; padding: 3px; border-radius:2px'
+    resetTag.innerText = 'PROVA ANCORA!';
+
+    setTimeout(function(){
+        resetTag.innerText="";
+        resetTag.style.padding = '0';
+    },3000);
+
+    counter.appendChild(resetTag);
 });
 
+//random image
+const N = 30;
+let imageArray = Array.from({length: N}, (_, index) => index + 1);
+imageArray = imageArray.map(i => i + '.webp');
+
+function getRandomImage(){
+    let randomIndex = Math.floor(Math.random() * imageArray.length);
+    let selectedImage = imageArray[randomIndex];
+    image.src = `./img/${selectedImage}`
+}
+
+plus.addEventListener('click', getRandomImage);
+minus.addEventListener('click', getRandomImage);
+reset.addEventListener('click', getRandomImage);
